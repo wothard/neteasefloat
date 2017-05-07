2fdfdc4df6dd275486c98c5b31cfb8dc650edcd5
wothard <ip16292132z@gmail.com>
2017-05-07 21:47:46 +0800

    更新数据库支持

--- float.py
+++ float.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 #coding: utf-8
+import MySQLdb
 from gpcharts import figure
-import musicc
-import music
+# import musicc
+# import music
 import uniout
 
 # 获取图像对象并设置x, y的值
 fig3 = figure()
-music.songcomment()
-musicc.songname()
-# print musicc.listsong[int(music.cl)-1:30+int(music.cl)-1]
-# print len(musicc.listsong[int(music.cl)-1:30+int(music.cl)-1])
-# print len(music.commentaq[:])
-# print (music.commentaq[:])
+songlist = []
+commentlist = []
+
+db = MySQLdb.connect("localhost", "root", "ip16292132z", "netfloat", charset='utf8')
+cursor = db.cursor()
+sql_value = "SELECT * FROM NETFLOATCT "
+try:
+    cursor.execute(sql_value)
+    results = cursor.fetchall()
+
+    for i in range(len(results)):
+        songlist.append((results[i][0].encode('utf-8')))
+        commentlist.append(int(results[i][1]))
+except:
+    print "Error : unable to fetch data!"
+db.close()
+# print songlist[:10]
+# print (commentlist[:10])
 xVals = ['Comment-numbers', 'Song-name']
-# # yVals = [['e','q','t','y','aa','ss','ccc'],[10,20,12,14,23,21,20]]
-yVals = [musicc.listsong[int(music.cl)-1:30+int(music.cl)-1], music.commentaq[:]]
+# yVals = [['e','q','t','y','aa','ss','ccc'],[10,20,12,14,23,21,20]]
+yVals = [songlist[:25], commentlist[:25]]
 fig3.title = 'netease歌曲评论图'
 fig3.ylabel = ''
 fig3.bar(xVals, yVals)
--- music.py
+++ music.py
@@ -10,64 +10,68 @@ import hashlib
 import requests
 import musicc
 
-def aesEncrypt(text, secKey):
-    pad = 16 - len(text) % 16
-    text = text + pad * chr(pad)
-    encryptor = AES.new(secKey, 2, '0102030405060708')
-    ciphertext = encryptor.encrypt(text)
-    ciphertext = base64.b64encode(ciphertext)
-    return ciphertext
+class TheKey(object):
+    """几个需要用到的解析网页js的函数"""
+    def aesEncrypt(self, text, secKey):
+        pad = 16 - len(text) % 16
+        text = text + pad * chr(pad)
+        encryptor = AES.new(secKey, 2, '0102030405060708')
+        ciphertext = encryptor.encrypt(text)
+        ciphertext = base64.b64encode(ciphertext)
+        return ciphertext
 
-def rsaEncrypt(text, pubKey, modulus):
-    text = text[::-1]
-    rs = int(text.encode('hex'), 16)**int(pubKey, 16) % int(modulus, 16)
-    return format(rs, 'x').zfill(256)
+    def rsaEncrypt(self, text, pubKey, modulus):
+        text = text[::-1]
+        rs = int(text.encode('hex'), 16)**int(pubKey, 16) % int(modulus, 16)
+        return format(rs, 'x').zfill(256)
 
-def createSecretKey(size):
-    return (''.join(map(lambda xx: (hex(ord(xx))[2:]), os.urandom(size))))[0:16]
+    def createSecretKey(self, size):
+        return (''.join(map(lambda xx: (hex(ord(xx))[2:]), os.urandom(size))))[0:16]
 
-def encryted_request(test):
-    text = json.dumps(text)
-    secKey = createSecretKey(16)
-    encText = aesEncrypt(aesEncrypt(text, nonce), secKey)
-    encSecKey = rsaEncrypt(secKey, pubKey, modulus)
-    data = {
-        'params': encText,
-        'encSecKey': encSecKey
-    }
-    return data
+    def encryted_request(self, test):
+        text = json.dumps(text)
+        secKey = createSecretKey(16)
+        encText = aesEncrypt(aesEncrypt(text, nonce), secKey)
+        encSecKey = rsaEncrypt(secKey, pubKey, modulus)
+        data = {
+            'params': encText,
+            'encSecKey': encSecKey
+        }
+        return data
+
+theKey = TheKey()
 commentaq = []
-print "你目前总共喜欢" + str(len(musicc.listx)) + "首歌曲"
-cl = raw_input("请选择从第几首查看评论（每次查看30首）:")
-cm = len(musicc.listx[int(cl)-1:int(cl)+29])
-def songcomment():
-    # url = 'http://music.163.com/weapi/v1/resource/comments/R_SO_4_1491585/?csrf_token='
-    headers = {
-        'Cookie': 'appver=1.5.0.75771;',
-        'Referer': 'http://music.163.com/'
-    }
-    text = {
-        'username': '邮箱',
-        'password': '密码',
-        'rememberLogin': 'true'
-    }
-    modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
-    nonce = '0CoJUm6Qyw8W8jud'
-    pubKey = '010001'
-    text = json.dumps(text)
-    secKey = createSecretKey(16)
-    encText = aesEncrypt(aesEncrypt(text, nonce), secKey)
-    encSecKey = rsaEncrypt(secKey, pubKey, modulus)
-    data = {
-        'params': encText,
-        'encSecKey': encSecKey
-    }
-    #获取歌曲评论数
-    for i in range(cm):
-        url = 'http://music.163.com/weapi/v1/resource/comments/R_SO_4_' + str(musicc.listx[i+int(cl)-1]) +'/?csrf_token='
-        req = requests.post(url, headers=headers, data=data)
-        commentaq.append(req.json()['total'])
-        # print str(musicc.listx[i+int(cl)])
+
+class GetCommentNum(object):
+    def songcomment(self):
+        headers = {
+            'Cookie': 'appver=1.5.0.75771;',
+            'Referer': 'http://music.163.com/'
+        }
+        text = {
+            'username': '邮箱',
+            'password': '密码',
+            'rememberLogin': 'true'
+        }
+        modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
+        nonce = '0CoJUm6Qyw8W8jud'
+        pubKey = '010001'
+        text = json.dumps(text)
+        secKey = theKey.createSecretKey(16)
+        encText = theKey.aesEncrypt(theKey.aesEncrypt(text, nonce), secKey)
+        encSecKey = theKey.rsaEncrypt(secKey, pubKey, modulus)
+        data = {
+            'params': encText,
+            'encSecKey': encSecKey
+        }
+        # 获取歌曲评论数
+        print "你目前总共喜欢" + str(len(musicc.listx)) + "首歌曲"
+        cm = len(musicc.listx)
+        for i in range(cm):
+            url = 'http://music.163.com/weapi/v1/resource/comments/R_SO_4_' + str(musicc.listx[i]) +'/?csrf_token='
+            req = requests.post(url, headers=headers, data=data)
+            commentaq.append(req.json()['total'])
 
 if '__name__' == '__main__':
-    songcomment()
+    t = GetCommentNum()
+    t.songcomment()
